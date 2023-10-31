b8cc2a7621d5bbca1838b005298c7f6e44f325e4
tolerevo <tolerevo@hotmail.com>
2023-10-31 10:38:11 -0600

    testimonio module done

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -9,8 +9,8 @@ function Testimonio(props) {
         src={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt='foto de Emma' />
       <div className='contenedor-texto-testimonio' >
-        <p className='nombre-testimonio' >{props.nombre} en {props.pais}</p>
-        <p className='cargo-testimonio' >{props.cargo} en {props.empresa}</p>
+        <p className='nombre-testimonio' ><strong>{props.nombre}</strong> in {props.pais}</p>
+        <p className='cargo-testimonio' >{props.cargo} in <strong>{props.empresa}</strong></p>
         <p className='texto-testimonio' >"{props.testimonio}"</p>
       </div>
     </div>
